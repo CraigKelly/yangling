0e3c8217344f5b79b73e01edbcfd5672e7f78ff5
Craig Kelly <craig.n.kelly@gmail.com>
2017-01-05 12:41:33 -0600

    business skel working

--- business.py
+++ business.py
@@ -2,12 +2,35 @@
 
 """Export business details from json file."""
 
+import sys
 import json
+
+
+def filt(rec):
+    """Return true if record should be written out."""
+    return False  # TODO
+
+
+def xlate(rec):
+    """Convert Python object to CSV record. Return headers if rec is None."""
+    if rec is None:
+        return "ID,Name"
+    return ""  # TODO
 
 
 def main():
     """Entry point."""
-    pass
+    sys.stdout.write(xlate(None) + '\n')
+
+    read, written = 0, 0
+    for line in sys.stdin:
+        rec = json.loads(line.strip())
+        read += 1
+        if filt(rec):
+            sys.stdout.write(xlate(rec) + '\n')
+            written += 1
+
+    sys.stderr.write("Read %d, Wrote %d\n" % (read, written))
 
 
 if __name__ == "__main__":
